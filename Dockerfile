ecab4fac799335b628d9cd1e6ede3ed1c67ef53a
mchome <fnatic.roach@gmail.com>
2016-02-15 21:16:27 +0800

    support ss mode

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # V2Ray Dockerfile
 #
-# Version: 1.1.0
+# Version: 1.2.0
 
 FROM ubuntu:14.04
 MAINTAINER fuyumi <280604399@qq.com>
@@ -9,6 +9,8 @@ MAINTAINER fuyumi <280604399@qq.com>
 ENV V2RAY_VERSION 1.7
 ENV V2RAY_PORT 9527
 ENV V2RAY_ALTERID 7259
+ENV SS_PORT 443
+ENV SS_PASSWD !~v2foobar~!
 
 # install git & curl & unzip & daemon
 RUN apt-get -qq update && \
@@ -19,8 +21,10 @@ RUN ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 # run install script
 RUN mkdir -p /usr/v2ray/
 ADD install-release.sh /usr/v2ray/install-release.sh
+ADD config.json /usr/v2ray/config.json
 RUN chmod +x /usr/v2ray/install-release.sh
 
 EXPOSE $V2RAY_PORT
+EXPOSE $SS_PORT
 
 CMD ["bash", "/usr/v2ray/install-release.sh"]
--- install-release.sh
+++ install-release.sh
@@ -32,7 +32,8 @@ chmod +x "/usr/bin/v2ray/v2ray"
 # Install V2Ray server config to /etc/v2ray
 mkdir -p /etc/v2ray
 if [ ! -f "/etc/v2ray/config.json" ]; then
-  cp "/tmp/v2ray/v2ray-${VER}-linux-${VDIS}/vpoint_vmess_freedom.json" "/etc/v2ray/config.json"
+  # cp "/tmp/v2ray/v2ray-${VER}-linux-${VDIS}/vpoint_vmess_freedom.json" "/etc/v2ray/config.json"
+  cp "/usr/v2ray/config.json" "/etc/v2ray/config.json"
 
   PORT=$V2RAY_PORT
   sed -i "s/17173/${PORT}/g" "/etc/v2ray/config.json"
@@ -43,15 +44,25 @@ if [ ! -f "/etc/v2ray/config.json" ]; then
   UUID=$(cat /proc/sys/kernel/random/uuid)
   sed -i "s/3b129dec-72a3-4d28-aeee-028a0fe86e22/${UUID}/g" "/etc/v2ray/config.json"
 
+  SS_PORT=$SS_PORT
+  sed -i "s/30001/${SS_PORT}/g" "/etc/v2ray/config.json"
+
+  SS_PASSWD=$SS_PASSWD
+  sed -i "s/v2rayss/${SS_PASSWD}/g" "/etc/v2ray/config.json"
+
   echo "PORT:${PORT}"
   echo "ALTERID:${ALTERID}"
   echo "UUID:${UUID}"
+  echo "SS_PORT:${SS_PORT}"
+  echo "SS_METHOD:aes-256-cfb"
+  echo "SS_PASSWD:${SS_PASSWD}"
 fi
 
 echo "========================================================================"
 echo "  Hey, you can use V2Ray Service now  "
-echo "  Version: $VER   ALTERID: $ALTERID  "
-echo "  Port: $PORT     UUID: $UUID  "
+echo "  Version: $VER   Port: $PORT  "
+echo "  ALTERID: $ALTERID     UUID: $UUID  "
+echo "  SS_PORT: $SS_PORT   SS_METHOD: aes-256-cfb   SS_PASSWD: $SS_PASSWD  "
 echo "========================================================================"
 
 /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json
